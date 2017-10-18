61bb99c42223ca5f59fcf7fb54ee8f3f72f76579
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2017-10-18 20:39:56 +0300

    Cleanup

--- SaitynoProjektasBackEnd/SaitynoProjektasBackEnd/Startup.cs
+++ SaitynoProjektasBackEnd/SaitynoProjektasBackEnd/Startup.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
